5a50076280b00f5a205bb3ef62de0fa85f8ef3c9
Mohammad Reza <mo.behzad34@gmail.com>
2024-12-04 11:20:20 +0330

    add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { BlogModule } from './blog/blog.module';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DatabaseModule, BlogModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    DatabaseModule,
+    BlogModule,
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
